f6c184d33269d76e813145baea5d3a43ab7280e7
lyndseyjw <lyndseywatson@Lyndseys-MacBook-Pro.local>
2021-09-19 10:37:36 -0700

    removed extra link that was added as a mistake

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,26 @@
+// creating a variable that saves the current date in specified format & displays on screen
 var today = moment().format('dddd, MMMM Do, YYYY');
 $('#currentDay').text(today);
 
+// variables for 7AM button & task input
 var button7 = $('.button7');
 var taskInput7 = $('.task7');
 
+// when user clicks the 7AM button, text input set to local storage
 button7.on('click', function() {
 
     var task7 = taskInput7.val();
     localStorage.setItem('7AM', task7);
 })
 
+// if there is a 7AM task saved in local storage, display to screen
 if (localStorage.getItem('7AM')) {
 
     var savedTask7 = localStorage.getItem('7AM');
     taskInput7.val(savedTask7);
 }
 
+// conditional statement that changes the calendar 
 if (moment().format("H") < 7) {
 
     taskInput7.addClass('future');
@@ -31,15 +36,18 @@ if (moment().format("H") < 7) {
     taskInput7.addClass('present');
 }
 
+// variables for 8AM button & task input
 var button8 = $('.button8');
 var taskInput8 = $('.task8');
 
+// when user clicks the 8AM button, text input set to local storage
 button8.on('click', function() {
 
     var task8 = taskInput8.val();
     localStorage.setItem('8AM', task8);
 })
 
+// if there is a 8AM task saved in local storage, display to screen
 if (localStorage.getItem('8AM')) {
 
     var savedTask8 = localStorage.getItem('8AM');
@@ -61,15 +69,18 @@ if (moment().format("H") < 8) {
     taskInput8.addClass('present');
 }
 
+// variables for 9AM button & task input
 var button9 = $('.button9');
 var taskInput9 = $('.task9');
 
+// when user clicks the 9AM button, text input set to local storage
 button9.on('click', function() {
 
     var task9 = taskInput9.val();
     localStorage.setItem('9AM', task9);
 })
 
+// if there is a 9AM task saved in local storage, display to screen
 if (localStorage.getItem('9AM')) {
 
     var savedTask9 = localStorage.getItem('9AM');
@@ -89,9 +100,11 @@ if (moment().format("H") < 9) {
     taskInput9.addClass('present');
 }
 
+// variables for 10AM button & task input
 var button10 = $('.button10');
 var taskInput10 = $('.task10');
 
+// when user clicks the 10AM button, text input set to local storage
 button10.on('click', function() {
 
     var task10 = taskInput10.val();
@@ -99,6 +112,7 @@ button10.on('click', function() {
     // taskInput10.attr('value', task10);
 })
 
+// if there is a 10AM task saved in local storage, display to screen
 if (localStorage.getItem('10AM')) {
 
     var savedTask10 = localStorage.getItem('10AM');
@@ -118,15 +132,18 @@ if (moment().format("H") < 10) {
     taskInput10.addClass('present');
 }
 
+// variables for 11AM button & task input
 var button11 = $('.button11');
 var taskInput11 = $('.task11');
 
+// when user clicks the 11AM button, text input set to local storage
 button11.on('click', function() {
 
     var task11 = taskInput11.val();
     localStorage.setItem('11AM', task11);
 })
 
+// if there is a 11AM task saved in local storage, display to screen
 if (localStorage.getItem('11AM')) {
 
     var savedTask11 = localStorage.getItem('11AM');
@@ -146,15 +163,18 @@ if (moment().format("H") < 11) {
     taskInput11.addClass('present');
 }
 
+// variables for 12PM button & task input
 var button12 = $('.button12');
 var taskInput12 = $('.task12');
 
+// when user clicks the 12PM button, text input set to local storage
 button12.on('click', function() {
 
     var task12 = taskInput12.value;
     localStorage.setItem('12PM', task12);
 })
 
+// if there is a 12PM task saved in local storage, display to screen
 if (localStorage.getItem('12PM')) {
 
     var savedTask12 = localStorage.getItem('12PM');
@@ -174,15 +194,18 @@ if (moment().format("H") < 12) {
     taskInput12.addClass('present');
 }
 
+// variables for 1PM button & task input
 var button13 = $('.button13');
 var taskInput13 = $('.task13');
 
+// when user clicks the 1PM button, text input set to local storage
 button13.on('click', function() {
 
     var task13 = taskInput13.value;
     localStorage.setItem('1PM', task13);
 })
 
+// if there is a 1PM task saved in local storage, display to screen
 if (localStorage.getItem('1PM')) {
 
     var savedTask13 = localStorage.getItem('1PM');
@@ -202,15 +225,18 @@ if (moment().format("H") < 13) {
     taskInput13.addClass('present');
 }
 
+// variables for 2PM button & task input
 var button14 = $('.button14');
 var taskInput14 = $('.task14');
 
+// when user clicks the 2PM button, text input set to local storage
 button14.on('click', function() {
 
     var task14 = taskInput14.value;
     localStorage.setItem('2PM', task14);
 })
 
+// if there is a 2PM task saved in local storage, display to screen
 if (localStorage.getItem('2PM')) {
 
     var savedTask14 = localStorage.getItem('2PM');
@@ -230,15 +256,18 @@ if (moment().format("H") < 14) {
     taskInput14.addClass('present');
 }
 
+// variables for 3PM button & task input
 var button15 = $('.button15');
 var taskInput15 = $('.task15');
 
+// when user clicks the 3PM button, text input set to local storage
 button15.on('click', function() {
 
     var task15 = taskInput15.value;
     localStorage.setItem('3PM', task15);
 })
 
+// if there is a 3PM task saved in local storage, display to screen
 if (localStorage.getItem('3PM')) {
 
     var savedTask15 = localStorage.getItem('3PM');
@@ -258,15 +287,18 @@ if (moment().format("H") < 15) {
     taskInput15.addClass('present');
 }
 
+// variables for 4PM button & task input
 var button16 = $('.button16');
 var taskInput16 = $('.task16');
 
+// when user clicks the 4PM button, text input set to local storage
 button16.on('click', function() {
 
     var task16 = taskInput16.value;
     localStorage.setItem('4PM', task16);
 })
 
+// if there is a 4PM task saved in local storage, display to screen
 if (localStorage.getItem('4PM')) {
 
     var savedTask16 = localStorage.getItem('4PM');
@@ -286,15 +318,18 @@ if (moment().format("H") < 16) {
     taskInput16.addClass('present');
 }
 
+// variables for 5PM button & task input
 var button17 = $('.button17');
 var taskInput17 = $('.task17');
 
+// when user clicks the 5PM button, text input set to local storage
 button17.on('click', function() {
 
     var task17 = taskInput17.value;
     localStorage.setItem('5PM', task17);
 })
 
+// if there is a 5PM task saved in local storage, display to screen
 if (localStorage.getItem('5PM')) {
 
     var savedTask17 = localStorage.getItem('5PM');
@@ -314,15 +349,18 @@ if (moment().format("H") < 17) {
     taskInput17.addClass('present');
 }
 
+// variables for 6PM button & task input
 var button18 = $('.button18');
 var taskInput18 = $('.task18');
 
+// when user clicks the 6PM button, text input set to local storage
 button18.on('click', function() {
 
     var task18 = taskInput18.value;
     localStorage.setItem('6PM', task18);
 })
 
+// if there is a 6PM task saved in local storage, display to screen
 if (localStorage.getItem('6PM')) {
 
     var savedTask18 = localStorage.getItem('6PM');
@@ -342,15 +380,18 @@ if (moment().format("H") < 18) {
     taskInput18.addClass('present');
 }
 
+// variables for 7PM button & task input
 var button19 = $('.button19');
 var taskInput19 = $('.task19');
 
+// when user clicks the 7PM button, text input set to local storage
 button19.on('click', function() {
 
     var task19 = taskInput19.value;
     localStorage.setItem('7PM', task19);
 })
 
+// if there is a 7PM task saved in local storage, display to screen
 if (localStorage.getItem('7PM')) {
 
     var savedTask19 = localStorage.getItem('7PM');
